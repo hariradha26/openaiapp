9aba743f092aef3fcba262fbcf5efc40a196c07f
hariradha26 <hariradhautpala@gmail.com>
2025-01-31 22:51:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st (https://itchronicles.com/wp-content/uploads/2020/11/where-is-ai-used.jpg ="AI-Powered Streamlit App")
+import streamlit as st ("https://itchronicles.com/wp-content/uploads/2020/11/where-is-ai-used.jpg" ="AI-Powered Streamlit App")
 
 import requests
 import json
